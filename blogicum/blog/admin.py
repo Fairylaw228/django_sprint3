3a2ee20b1098a7ac4b62073f1152ea26abb0f795
Fairylaw228 <sasha87255@gmail.com>
2024-12-21 05:42:36 +0300

    ура

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -5,55 +5,29 @@ from django.utils.translation import gettext_lazy as _
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
-    'title', 
-    'author', 
-    'pub_date', 
-    'category', 
-    'location', 
-    'is_published'
-    )
-    list_filter = ('category', 'is_published')
-    search_fields = ('title', 'text')
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'is_published')
-    search_fields = ('title',)
-
-
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_published')
-    search_fields = ('name',)
-
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Location, LocationAdmin)
-
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = (
-    'title', 
-    'author', 
-    'pub_date', 
-    'category', 
-    'location', 
-    'is_published'
+        'title',
+        'author',
+        'pub_date',
+        'category',
+        'location',
+        'is_published'
     )
     list_filter = ('category', 'is_published')
     search_fields = ('title', 'text')
     fieldsets = (
         (None, {
-    'fields': (
-        'title', 
-        'text', 
-        'author', 
-        'category', 
-        'location', 
-        'is_published'
-    )
-    }),
-        (_('Publication Info'), {'fields': ('pub_date', 'created_at')}),
+            'fields': (
+                'title',
+                'text',
+                'author',
+                'category',
+                'location',
+                'is_published'
+            )
+        }),
+        (_('Publication Info'), {
+            'fields': ('pub_date', 'created_at')
+        }),
     )
     verbose_name = _('публикация')
     verbose_name_plural = _('публикации')
@@ -71,4 +45,8 @@ class LocationAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     verbose_name = _('местоположение')
     verbose_name_plural = _('местоположения')
-    
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
